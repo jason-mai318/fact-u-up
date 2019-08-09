b1f0b751e24b101cc8824f4f8eb189d6aa3002a4
Jason <jasonmai318@gmail.com>
2019-08-09 02:48:43 -0400

    Updates

---
+++ precache-manifest.5f64cccb3301520610f2a9dcf51a6afc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1f0cd5dc693482fd12040c23e3b9bd",
+    "url": "/yearFacts/index.html"
+  },
+  {
+    "revision": "a3c2249dc6fc912188a8",
+    "url": "/yearFacts/static/css/main.3073cfb7.chunk.css"
+  },
+  {
+    "revision": "ba679d9d986332c76673",
+    "url": "/yearFacts/static/js/2.7b1aaa3f.chunk.js"
+  },
+  {
+    "revision": "a3c2249dc6fc912188a8",
+    "url": "/yearFacts/static/js/main.00269837.chunk.js"
+  },
+  {
+    "revision": "e97c90aecae60161de68",
+    "url": "/yearFacts/static/js/runtime~main.e2aa2717.js"
+  }
+]);
